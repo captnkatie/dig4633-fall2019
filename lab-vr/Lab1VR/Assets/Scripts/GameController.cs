0c737216a3140eba108883773e7e5e231b014cc0
captnkatie <46755399+captnkatie@users.noreply.github.com>
2019-09-25 17:36:41 -0400

    Update!
    Pleasee

---
+++ lab-vr/Lab1VR/Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public bool isappleplaced1;
+    public bool isappleplaced2;
+
+    public void checkForVictory() {
+
+        if(isappleplaced1 && isappleplaced2)
+        {
+            Debug.Log("You got em all captn");
+        }
+    }
+
+    public void applewasplaced()
+    {
+        this.isappleplaced1 = true;
+        Debug.Log("Apple is placed!");
+        checkForVictory();
+    }
+    public void applewasplacedtwo()
+    {
+        this.isappleplaced2 = true;
+        Debug.Log("Apple2 is placed!");
+        checkForVictory();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
